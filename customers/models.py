eca836b8c4840bbb13080c15259744ff3b611c1d
PancirDev <pancir.dev@gmail.com>
2021-09-27 16:08:05 +0300

    admin init

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    company = models.CharField(max_length=150, unique=True)
+    full_name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=17)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    note = models.CharField(max_length=300)
