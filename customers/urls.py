70764d85bc447f48f984f3c801ae836f1d4caed7
PancirDev <pancir.dev@gmail.com>
2021-09-29 15:25:37 +0300

    start working. save and continue

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import CustomerDetailView
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('home/', views.home, name='home'),
+    path('detail/<int:pk>/', views.det, name='det'),
+    path('<int:pk>/', CustomerDetailView.as_view(), name='detail'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,18 +1,27 @@
 from django.http import HttpResponse
-#from django.shortcuts import render
-
-#from customers.models import Customer
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Customer
 
 #__all__ = (
 #    'home',
 #)
 
 
-#def home(request):
-#    qs = Customer.objects.all()
-#    context = {'objects_list': qs}
-#    return render(request, 'customers/home.html', context)
+def home(request):
+    qs = Customer.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'customers/home.html', context)
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. I'm index | path('' ")
+
+
+def det(request, pk=None):
+    return HttpResponse("Hello, world. I'm det | path('detail/<int:pk>/' ")
+
+
+class CustomerDetailView(DetailView):
+    queryset = Customer.objects.all()
+    template_name = 'customers/detail.html'
