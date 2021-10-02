425713746fbd1cced0e9123064e2f6a98d75dd5d
PancirDev <pancir.dev@gmail.com>
2021-10-02 16:37:05 +0300

    temp. before project_detail.html changing

--- customers/urls.py
+++ customers/urls.py
@@ -6,4 +6,6 @@ app_name = 'customers'
 urlpatterns = [
     path('', views.CustomerListView.as_view(), name='list'),
     path('<int:pk>/', views.CustomerDetailView.as_view(), name='detail'),
+    path('projects/', views.ProjectListView.as_view(), name='projects_list'),
+    path('projects/<int:pk>', views.ProjectDetailView.as_view(), name='project_detail')
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,13 +1,28 @@
 from django.views.generic import DetailView, ListView
-from .models import Customer
+from .models import Customer, Project
 
 
 class CustomerDetailView(DetailView):
     model = Customer
-    template_name = 'customers/customer_detail.html'
+    # template_name = 'customers/customer_detail.html'
+    # context_object_name = 'customer'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['project_list'] = Project.objects.filter(customer=context['customer'])
+        return context
 
 
 class CustomerListView(ListView):
     model = Customer
-    template_name = 'customers/customer_list.html'
+    # template_name = 'customers/customer_list.html'
+    context_object_name = 'customers_list'
 
+
+class ProjectListView(ListView):
+    model = Project
+    context_object_name = 'projects_list'
+
+
+class ProjectDetailView(DetailView):
+    model = Project
