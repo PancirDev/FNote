a436cc54c5f7ac1294e165f953a477156b426a57
PancirDev <pancir.dev@gmail.com>
2021-10-02 14:25:24 +0300

    temp. before massive view changing

--- customers/views.py
+++ customers/views.py
@@ -4,7 +4,10 @@ from .models import Customer
 
 class CustomerDetailView(DetailView):
     model = Customer
+    template_name = 'customers/customer_detail.html'
 
 
 class CustomerListView(ListView):
     model = Customer
+    template_name = 'customers/customer_list.html'
+
