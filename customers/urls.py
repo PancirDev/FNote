5b30cd96eb05f9d42bac5bdd0fa02b261ed6bf19
PancirDev <pancir.dev@gmail.com>
2021-10-11 00:42:26 +0300

    Customers Stable. Cleaned

--- customers/urls.py
+++ customers/urls.py
@@ -6,8 +6,8 @@ app_name = 'customers'
 urlpatterns = [
     path('', views.CustomerListView.as_view(), name='list'),
     path('create/', views.CustomerCreate.as_view(), name='create'),
-    path('update/<int:pk>/', views.CustomerUpdate.as_view(), name='update'),
-    path('delete/<int:pk>/', views.CustomerDelete.as_view(), name='delete'),
+    path('<int:pk>/update/', views.CustomerUpdate.as_view(), name='update'),
+    path('<int:pk>/delete/', views.CustomerDelete.as_view(), name='delete'),
     path('projects/', views.ProjectListView.as_view(), name='projects_list'),
     path('projects/<int:pk>', views.ProjectDetailView.as_view(), name='project_detail')
 ]
